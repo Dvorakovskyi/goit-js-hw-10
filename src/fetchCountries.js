bb71ef88142162696813e30f583a6e572d9c5187
Volodymyr <vladimir.dvorakovskyi@gmail.com>
2023-03-26 19:27:54 +0300

    Update code

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,6 @@
 export class RestCountriesAPI {
     #BASE_URL = 'https://restcountries.com';
-    #BASE_PARAMS = 'fields=name;fields=capital;fields=population;fields=flags;fields=languages';
+    #BASE_PARAMS = 'fields=name,capital,population,flags,languages';
 
     fetchCountries(name) {
         return fetch(`${this.#BASE_URL}/v3.1/name/${name}?${this.#BASE_PARAMS}`)
--- src/index.js
+++ src/index.js
@@ -1,23 +1,47 @@
 import './css/styles.css';
 import { RestCountriesAPI } from './fetchCountries';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
 
-
-
-
-
 const inputEl = document.querySelector('#search-box');
+const contryListEl = document.querySelector('.country-list');
 
 const restCountriesAPI = new RestCountriesAPI();
 
-const handleSearchCountries = (event) => {
-    const searchCountry = event.target.value;
+const handleSearchCountries = event => {
+  const searchCountry = event.target.value;
 
-    restCountriesAPI.fetchCountries(searchCountry)
+    restCountriesAPI
+        .fetchCountries(searchCountry)
         .then(data => {
-            console.log(data[0]);
-    })
+            const countryInfo = data
+                .map((data) => `<li>
+          <h2><img src=${data.flags.svg} width="20" height="15"> ${data.name.official}</h2>
+          <p>Capital: ${data.capital}</p>
+          <p>Population: ${data.population}</p>
+          <p>Languages: ${Object.values(data.languages)}</p>
+      </li>`)
+                .join('');
+            contryListEl.innerHTML = countryInfo;
+        });
+  
 };
 
-inputEl.addEventListener('input', handleSearchCountries);
+// function renderCountryCard(data) {
+//   const countryInfo = data
+//     .map(data => `<li>
+//         <h2><img src=${data.flags.svg} width="20" height="15"> ${data.name.official}</h2>
+//         <p>Capital: ${data.capital}</p>
+//         <p>Population: ${data.population}</p>
+//         <p>Languages: ${Object.values(data.languages)}</p>
+//     </li>`
+//     )
+//     .join('');
+//     contryListEl.innerHTML = countryInfo;
+// }
+
+inputEl.addEventListener(
+  'input',
+  debounce(handleSearchCountries, DEBOUNCE_DELAY)
+);
